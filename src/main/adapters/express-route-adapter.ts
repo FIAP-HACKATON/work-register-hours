99e4285e1d350bf0d14008391705cae2571f0a7d
Gustavo Mendes de Oliveira <Gustavo@Macecofvhglqg1q.local>
2024-03-22 19:49:14 -0300

    feta: verify token

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -4,19 +4,22 @@ import { HttpRequest } from '../../infra/http/interfaces/HttpRequest';
 
 export const expressRouteAdapter =
   (controller: BaseController) => async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body,
-      query: req.query,
-      params: req.params,
-      headers: req.headers,
-    };
-
-    const httpResponse = await controller.handle(httpRequest);
-    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      res.status(httpResponse.statusCode).json(httpResponse.body);
-    } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body?.message,
-      });
+    try {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+        query: req.query,
+        params: req.params,
+        headers: req.headers,
+      };
+      const httpResponse = await controller.handle(httpRequest);
+      if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+        res.status(httpResponse.statusCode).json(httpResponse.body);
+      } else {
+        res.status(httpResponse.statusCode).json({
+          error: httpResponse.body?.message,
+        });
+      }
+    } catch (err) {
+      //TODO
     }
   };
--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -7,16 +7,17 @@ import { makeGetUserByFiltersController } from '../factories/controllers/user/ge
 import { makeUpdateUserController } from '../factories/controllers/user/update-user/controller-factory';
 import { makeDeleteUserController } from '../factories/controllers/user/delete-user/controller-factory';
 import { makeGetUserAccessController } from '../factories/controllers/user/get-user-access/controller-factory';
+import { expressMiddlewareAccess } from '../../main/adapters/express-middleware-access';
 
 export default (router: Router): void => {
   router.post('/user/', expressRouteAdapter(makeCreateUserController()));
   router.get('/access', expressRouteAdapter(makeGetUserAccessController()));
-  router.get('/users/', expressRouteAdapter(makeGetUsersController()));
+  router.get('/users/', expressMiddlewareAccess(), expressRouteAdapter(makeGetUsersController()));
   router.get(
-    '/user/filters',
+    '/user/filters', expressMiddlewareAccess(),
     expressRouteAdapter(makeGetUserByFiltersController()),
   );
-  router.get('/user/:id', expressRouteAdapter(makeGetUserByIdController()));
-  router.patch('/user/:id', expressRouteAdapter(makeUpdateUserController()));
-  router.delete('/user/:id', expressRouteAdapter(makeDeleteUserController()));
+  router.get('/user/:id', expressMiddlewareAccess(), expressRouteAdapter(makeGetUserByIdController()));
+  router.patch('/user/:id', expressMiddlewareAccess(), expressRouteAdapter(makeUpdateUserController()));
+  router.delete('/user/:id', expressMiddlewareAccess(), expressRouteAdapter(makeDeleteUserController()));
 };
